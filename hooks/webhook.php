c18677208497d666b3ea82c323f9e10a1aaa95c7
Your name <you@example.com>
2018-11-16 17:40:32 +0800

    1115

--- hooks/webhook.php
+++ hooks/webhook.php
@@ -26,11 +26,11 @@
 
     // 判断签名是否匹配  
     if ($hash === $payloadHash) {
-        $cmd = "cd $target && git pull";
+        $cmd = "cd $target && git pull origin master";
         $res = shell_exec($cmd);
 
         $res_log .= 'Success:'.PHP_EOL;
-        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL . 'cmd' . $cmd. PHP_EOL . 'res' . $res. PHP_EOL;
         $res_log .= $res.PHP_EOL;
         $res_log .= '======================================================================='.PHP_EOL;
 
