b6160c5d2b7f5e599d5ba152d120e5713fab9287
Your name <you@example.com>
2018-11-16 18:28:14 +0800

    1115

--- hooks/webhook.php
+++ hooks/webhook.php
@@ -26,23 +26,16 @@
 
     // 判断签名是否匹配  
     if ($hash === $payloadHash) {
-        $cmd1 = "cd ".$target;
-        $res = shell_exec($cmd1);
-        $cmd2 = "git checkout master --quiet";
-        $res = shell_exec($cmd2);
-        $cmd3 = "git fetch -p --all --quiet";
-        $res = shell_exec($cmd3);
-        $cmd4 = "git reset --hard origin/master --quiet";
-        $res = shell_exec($cmd4);
+        $cmd = "cd " . $target . " && git pull origin master";
+        $res = shell_exec($cmd);
 
         $res_log .= 'Success:'.PHP_EOL;
-        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL . 'cmd1:' . $cmd1. PHP_EOL. 'cmd2:' . $cmd2. PHP_EOL. 'cmd3:' . $cmd3. PHP_EOL. 'cmd4:' . $cmd4. PHP_EOL . 'res' . $res. PHP_EOL;
+        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL . 'cmd:' . $cmd . PHP_EOL;
         $res_log .= $res.PHP_EOL;
         $res_log .= '======================================================================='.PHP_EOL;
 
         fwrite($fs, $res_log);
         $fs and fclose($fs);
-
 
       } else {
             $res_log  = 'Error:'.PHP_EOL;
