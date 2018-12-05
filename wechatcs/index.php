d665207f6ee13324b63c96d48f8418b47eea37af
Your name <you@example.com>
2018-12-05 18:01:15 +0800

    1205

--- wechatcs/index.php
+++ wechatcs/index.php
@@ -6,7 +6,7 @@
 //define your token
 define("TOKEN", "yancycs");
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+// $wechatObj->valid();
 $wechatObj->responseMsg();//调用回复消息的函数
  
 class wechatCallbackapiTest
@@ -26,32 +26,43 @@ class wechatCallbackapiTest
 
    public function responseMsg()
     {
-        $$postStr = $$GLOBALS["HTTP_RAW_POST_DATA"];
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-        if (!empty($$postStr)){
+        if (!empty($postStr)){
                 libxml_disable_entity_loader(true);
-                  $$postObj = simplexml_load_string($$postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $$fromUsername = $$postObj->FromUserName;
-                $$toUsername = $$postObj->ToUserName;
-                $$keyword = trim($$postObj->Content);
-                $$time = time();
-                $$textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName>
+                  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName>
                             <FromUserName><![CDATA[%s]]></FromUserName>
                             <CreateTime>%s</CreateTime>
                             <MsgType><![CDATA[text]]></MsgType>
                             <Content><![CDATA[%s]]></Content>
                             <FuncFlag>0</FuncFlag>
-                            </xml>";             
-if($$postObj->MsgType=='event'){
-  if($$postObj->Event == 'CLICK'){
-    if($$postObj->EventKey == 'TEST_V2_1'){
+                            </xml>";        
+                if(!empty( $keyword ))
+                        {
+                          $msgType = "text";
+                          $contentStr = "你好！！";
+                          $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                          echo $resultStr;
+                        }else{
+                          echo "Input something...";
+                        }
+         
+                }     
+                if($postObj->MsgType=='event'){
+                  if($postObj->Event == 'CLICK'){
+                    if($postObj->EventKey == 'TEST_V2_1'){
 
- $$contentStr = "微信连,www.phpos.net";
- $$resultStr = sprintf($$textTpl, $$fromUsername, $$toUsername, $$time, $$contentStr);
- echo $$resultStr;
-   }
-  }
-}
+                 $contentStr = "微信连,www.phpos.net";
+                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $contentStr);
+                 echo $resultStr;
+                   }
+                  }
+                }
 
     }else {
         echo "success";
