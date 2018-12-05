a52bddda3b9be38d8fe80245e6fd00b42549a34d
Your name <you@example.com>
2018-12-05 17:46:46 +0800

    a

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -31,35 +31,35 @@ class IndexAction extends CommonAction {
             'appid' => C('APP_ID'), //填写高级调用功能的app id
             'appsecret' => C('APP_SECRET'), //填写高级调用功能的密钥
         );
-        $this->wechat_obj = new Wechat($options);
-        $list = $this->wechat_obj->getMenu();
+        $list = $this->wechat_obj = new Wechat($options);
+        // $list = $this->wechat_obj->getMenu();
 
-      $button = array (
-          'button' => array (
-            0 => array (
-              'type' => 'click',
-              'name' => '测试1',
-              'key' => 'TEST_V1_1',
-            ),
-            1 => array (
-              'name' => '测试2',
-              'sub_button' => array (
-                  0 => array (
-                    'type' => 'click',
-                    'name' => '测试2',
-                    'key' => 'TEST_V2_1',
-                  ),
-                  1 => array (
-                    'type' => 'view',
-                    'name' => '测试3',
-                    'url' => 'http://baidu.com',
-                  ),
-              ),
-            ),
-          ),
-      );
+          // $button = array (
+          //     'button' => array (
+          //       0 => array (
+          //         'type' => 'click',
+          //         'name' => '测试1',
+          //         'key' => 'TEST_V1_1',
+          //       ),
+          //       1 => array (
+          //         'name' => '测试2',
+          //         'sub_button' => array (
+          //             0 => array (
+          //               'type' => 'click',
+          //               'name' => '测试2',
+          //               'key' => 'TEST_V2_1',
+          //             ),
+          //             1 => array (
+          //               'type' => 'view',
+          //               'name' => '测试3',
+          //               'url' => 'http://baidu.com',
+          //             ),
+          //         ),
+          //       ),
+          //     ),
+          // );
 
-        $list = $this->wechat_obj->createMenu($button);
+        // $list = $this->wechat_obj->createMenu($button);
         p($list);
         echo 'test';
         die;
