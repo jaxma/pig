716e40338d22af797ec19ed761ad1a8ddbd3d90e
Your name <you@example.com>
2018-12-05 17:57:06 +0800

    a

--- wechatcs/index.php
+++ wechatcs/index.php
@@ -22,45 +22,43 @@ class wechatCallbackapiTest
           exit;
         }
     }
- 
-    public function responseMsg()
+
+
+   public function responseMsg()
     {
-    //get post data, May be due to the different environments
-    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
- 
- 
-        //extract post data
-    if (!empty($postStr)){
-                
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-              <ToUserName><![CDATA[%s]]></ToUserName>
-              <FromUserName><![CDATA[%s]]></FromUserName>
-              <CreateTime>%s</CreateTime>
-              <MsgType><![CDATA[%s]]></MsgType>
-              <Content><![CDATA[%s]]></Content>
-              <FuncFlag>0</FuncFlag>
-              </xml>";             
-        if(!empty( $keyword ))
-                {
-                  $msgType = "text";
-                  $contentStr = "Welcome to wechat world!";
-                  $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                  echo $resultStr;
-                }else{
-                  echo "Input something...";
-                }
- 
-        }else {
-          echo "";
-          exit;
-        }
+        $$postStr = $$GLOBALS["HTTP_RAW_POST_DATA"];
+
+        if (!empty($$postStr)){
+                libxml_disable_entity_loader(true);
+                  $$postObj = simplexml_load_string($$postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $$fromUsername = $$postObj->FromUserName;
+                $$toUsername = $$postObj->ToUserName;
+                $$keyword = trim($$postObj->Content);
+                $$time = time();
+                $$textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[text]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            <FuncFlag>0</FuncFlag>
+                            </xml>";             
+if($$postObj->MsgType=='event'){
+  if($$postObj->Event == 'CLICK'){
+    if($$postObj->EventKey == 'TEST_V2_1'){
+
+ $$contentStr = "微信连,www.phpos.net";
+ $$resultStr = sprintf($$textTpl, $$fromUsername, $$toUsername, $$time, $$contentStr);
+ echo $$resultStr;
+   }
+  }
+}
+
+    }else {
+        echo "success";
+        exit;
     }
-    
+}
+
   private function checkSignature()
   {
         $signature = $_GET["signature"];
