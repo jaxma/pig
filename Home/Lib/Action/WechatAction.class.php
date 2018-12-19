5ecb1542d5828aa78ca5b3bf6b0b84dfbc76c1b2
Your name <you@example.com>
2018-12-19 16:26:00 +0800

    1219

--- Home/Lib/Action/WechatAction.class.php
+++ Home/Lib/Action/WechatAction.class.php
@@ -100,6 +100,7 @@ class WechatAction extends CommonAction {
         if($Event['event'] == 'CLICK'){
             $key = $Event['key'];
             $key = trim($key);
+            $key = 'MENU_20181219_84519';
             $where = "key like '%$key%'";
             $res = M('wechat_menu')->where($where)->find();
             // $res = M('wechat_menu')->where('id = 874')->find();
