98bed16ffb445d17c746926dfa7aae77369d2fd0
Your name <you@example.com>
2018-12-20 09:23:49 +0800

    1220

--- ThinkPHP/Extend/Library/ORG/Wechat/Wechat.class.php
+++ ThinkPHP/Extend/Library/ORG/Wechat/Wechat.class.php
@@ -1114,7 +1114,44 @@ class Wechat
 	 * @param boolean $post_file 是否文件上传
 	 * @return string content
 	 */
-	private function http_post($url,$param,$post_file = false,$img_path = ''){
+	private function http_post($url,$param,$post_file = false){
+		$oCurl = curl_init();
+		if(stripos($url,"https://")!==FALSE){
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+		}
+		if (is_string($param) || $post_file) {
+			$strPOST = $param;
+		} else {
+			$aPOST = array();
+			foreach($param as $key=>$val){
+				$aPOST[] = $key."=".urlencode($val);
+			}
+			$strPOST =  join("&", $aPOST);
+		}
+		curl_setopt($oCurl, CURLOPT_URL, $url);
+		curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt($oCurl, CURLOPT_POST,true);
+		curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+		$sContent = curl_exec($oCurl);
+		$aStatus = curl_getinfo($oCurl);
+		curl_close($oCurl);
+		if(intval($aStatus["http_code"])==200){
+			return $sContent;
+		}else{
+			return false;
+		}
+	}    
+
+	/**
+	 * POST 请求
+	 * @param string $url
+	 * @param array $param
+	 * @param boolean $post_file 是否文件上传
+	 * @return string content
+	 */
+	private function http_post2($url,$param,$post_file = false,$img_path = ''){
 		$oCurl = curl_init();
 		if(stripos($url,"https://")!==FALSE){
 			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
@@ -1151,7 +1188,6 @@ class Wechat
 			return false;
 		}
 	}    
-
 	/**
 	 * 设置缓存，按需重载
 	 * @param string $cachename
@@ -1745,7 +1781,7 @@ class Wechat
 	public function uploadImg($data,$img_path = ''){
 		if (!$this->access_token && !$this->checkAuth()) return false;
 		//原先的上传多媒体文件接口使用 self::UPLOAD_MEDIA_URL 前缀
-		$result = $this->http_post(self::API_URL_PREFIX.self::MEDIA_UPLOADIMG_URL.'access_token='.$this->access_token,$data,true,$img_path);
+		$result = $this->http_post2(self::API_URL_PREFIX.self::MEDIA_UPLOADIMG_URL.'access_token='.$this->access_token,$data,true,$img_path);
 		if ($result)
 		{
 			$json = json_decode($result,true);
@@ -1804,6 +1840,15 @@ class Wechat
 		 $real_path= $file_info['filename'];
 		 //$real_path=str_replace("/", "\\", $real_path);
 		 $data= array("media"=>"@{$real_path}",'form-data'=>$file_info);
+        if (class_exists('\CURLFile')) {
+			$data['media'] = new \CURLFile(realpath($real_path));
+			$data['form'] = $file_info;
+		} else {
+		     if (defined('CURLOPT_SAFE_UPLOAD')) {
+		     		//5.6是没有的，设置了也无法强制用旧语法，5.5是可以的
+		         curl_setopt($ch1,CURLOPT_SAFE_UPLOAD,FALSE);
+		     }
+		}
 		 curl_setopt( $ch1, CURLOPT_URL, $url );
 		 curl_setopt( $ch1, CURLOPT_POST, 1 );
 		 curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, 1 );
